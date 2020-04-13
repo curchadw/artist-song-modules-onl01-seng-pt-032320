82555b6a0adfb678c3b35e4dd39ecf4870a3f1d0
Curtis Chadwell <laurencechadwell@gmail.com>
2020-04-13 20:54:17 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,7 +4,7 @@ require_relative '../lib/concerns/memorable'
 class Artist
   extend Memorable::ClassMethods
   extend Findable::ClassMethods
-
+  include Paramable
   attr_accessor :name
   attr_reader :songs
 
@@ -40,7 +40,7 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
 end
