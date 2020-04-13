6a89b2e45e9a3cc0ff7fc306fb8b18dac9a0b849
Curtis Chadwell <laurencechadwell@gmail.com>
2020-04-13 20:53:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,7 +4,7 @@ require_relative '../lib/concerns/memorable'
 class Song
   extend Memorable::ClassMethods
   extend Findable::ClassMethods
-
+  include Paramable
   attr_accessor :name
   attr_reader :artist
 
