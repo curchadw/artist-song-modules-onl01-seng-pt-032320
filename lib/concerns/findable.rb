f3f0fb43fbbb6e98b3fb7086af1998eecc7a11e6
Curtis Chadwell <laurencechadwell@gmail.com>
2020-04-13 20:27:57 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -4,20 +4,20 @@ module Findable
     def self.all
       @@artists
     end
-    #
+    
     def self.all
       @@songs
     end
 
 
 
-    def find_by_name(name)
-      @@songs.detect{|a| a.name == name}
-    end
-
-    def find_by_name(name)
-      @@artists.detect{|a| a.name == name}
-    end
+    # def find_by_name(name)
+    #   @@songs.detect{|a| a.name == name}
+    # end
+    # 
+    # def find_by_name(name)
+    #   @@artists.detect{|a| a.name == name}
+    # end
 
  end
 
